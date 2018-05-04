03ca615baba502116a728531a9b132e173cb8dec
Ewulino <36926607+Ewulino@users.noreply.github.com>
2018-05-04 21:44:55 +0200

    Update 2.feladat.py

--- 2.feladat.py
+++ 2.feladat.py
@@ -1,7 +1,7 @@
 
 n=input('adj meg egy számot: ')
-b1=(input('add meg a szám számrendszerét: '))
-b2 =(input('adj meg egy másik számrendszert,amibe konvertálni szeretnéd: '))
+b1=int(input('add meg a szám számrendszerét: '))
+b2 =int(input('adj meg egy másik számrendszert,amibe konvertálni szeretnéd: '))
 def tizesbe(n,b1):
     ossz=0
     n=n[::-1]
