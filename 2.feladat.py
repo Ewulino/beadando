68410edaf150d26ccb4231e746b5ce7a33bbb226
Ewulino <36926607+Ewulino@users.noreply.github.com>
2018-05-05 19:28:42 +0200

    Add files via upload

--- 2.feladat.py
+++ 2.feladat.py
@@ -1,7 +1,8 @@
 
 n=input('adj meg egy számot: ')
-b1=int(input('add meg a szám számrendszerét: '))
-b2 =int(input('adj meg egy másik számrendszert,amibe konvertálni szeretnéd: '))
+b1=int(input('adj meg a szám számrendszerét: '))
+b2 = int(input('adj meg egy másik számrendszert,amibe konvertálni szeretnéd: '))
+
 def tizesbe(n,b1):
     ossz=0
     n=n[::-1]
@@ -9,4 +10,14 @@ def tizesbe(n,b1):
         ujsz=int(n[i])*(b1**i)
         ossz+=ujsz
     return ossz
-print(tizesbe(n,b1))
+print(tizesbe(sz,b1))
+sz=tizesbe(n,b1)
+def convert(sz,b2):
+        new=[]
+        while sz!=0:
+            maradek=sz%b2
+            new.append(maradek)
+            sz=sz//b2
+        for i in range(len(new)-1,-1,-1):
+            print(new[i],end='')
+convert(sz,b2)
