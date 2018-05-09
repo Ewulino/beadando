1ab7ba24ab0c6cc3d7486134546670dafbac7e20
Ewulino <36926607+Ewulino@users.noreply.github.com>
2018-05-09 18:45:37 +0200

    Update 1.feladat.py

--- 1.feladat.py
+++ 1.feladat.py
@@ -1,7 +1,8 @@
+# első 100 fibonacchi szám listája
 def fibo():
-    ls = [0, 1]
+    ls = [0, 1]   # fibonacchi számok 0. eleme, 1.eleme
     while len(ls)!=100:
-        a=ls[-1]+ls[-2]
+        a=ls[-1]+ls[-2]     # megelőző 2 szám összegéből jön létre
         ls.append(a)
     return ls
 print(fibo())
